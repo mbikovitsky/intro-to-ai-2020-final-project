6b064fb5a208ac1c12b379d7932be321e25c50e6
Michael Bikovitsky <biko@bikodbg.com>
2020-07-09 17:02:55 +0300

    Implement k-mer counting script

---
+++ kmercount.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3.7
+# -*- coding: utf-8 -*-
+
+import argparse
+import sys
+from typing import Dict
+
+import pandas as pd
+from joblib import Parallel, delayed
+from skbio import Sequence
+
+
+def main():
+    args = parse_command_line()
+
+    sequences = pd.read_table(
+        args.sequences,
+        header=None,
+        names=["id", "sequence"],
+        dtype={"id": str},
+        converters={"sequence": lambda string: string.upper()},
+    )
+
+    results = Parallel(n_jobs=args.jobs, verbose=10)(
+        delayed(kmercount)(row["sequence"], args.k) for _, row in sequences.iterrows()
+    )
+
+    print("*** Merging results... ***", file=sys.stderr)
+    df = pd.DataFrame(
+        data=results,
+        index=sequences["id"],
+        dtype=pd.SparseDtype(dtype="uint64", fill_value=pd.NA),
+    )
+
+    print("*** Saving data... ***", file=sys.stderr)
+    df.to_csv(args.output, na_rep="0")
+
+
+def kmercount(sequence: str, k: int) -> Dict[str, int]:
+    return Sequence(sequence).kmer_frequencies(k)
+
+
+def parse_command_line() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("sequences", help="File containing the sequences to process")
+    parser.add_argument("output", help="Output filename (CSV)")
+    parser.add_argument("k", type=int, help="k-mer length")
+    parser.add_argument(
+        "-j", "--jobs", type=int, default=-1, help="Number of parallel jobs"
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
