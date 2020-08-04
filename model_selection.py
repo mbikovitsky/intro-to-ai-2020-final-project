ac3b10188427e0675dac05c062a89306cb6885d4
Michael Bikovitsky <biko@bikodbg.com>
2020-08-04 16:54:28 +0300

    Switch to randomized CV for model selection

--- model_selection.py
+++ model_selection.py
@@ -9,10 +9,10 @@ import numpy as np
 import torch
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from torch import from_numpy, nn
 from torch.utils.data import DataLoader, TensorDataset
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
 
 from neural_net import Net
 from preprocessing import one_hot_encode_sequences, read_all_data
@@ -77,18 +77,22 @@ class NNEstimator(BaseEstimator, RegressorMixin):
             return self._network(X).numpy().reshape(-1)
 
 
-def grid_search(X, y, training_epochs: int) -> Dict[str, int]:
-    param_grid = {
-        # "stage1_conv_channels": range(96, 101),
-        # "stage1_conv_kernel_size": range(7, 12),
-        "stage2_conv_kernel_size": [3, 5, 7],
-        "stage3_pool_kernel_size": [10, 11],
+def randomized_search(
+    X, y, training_epochs: int, iterations: int, jobs: int
+) -> Dict[str, int]:
+    param_distributions = {
+        "stage1_conv_channels": range(96, 101),
+        "stage1_conv_kernel_size": range(7, 18),
+        "stage2_conv_kernel_size": range(3, 10, 2),
+        "stage3_pool_kernel_size": range(8, 13),
         "stage4_conv_channels": range(196, 201),
     }
-    search = GridSearchCV(
+    search = RandomizedSearchCV(
         estimator=NNEstimator(),
-        param_grid=param_grid,
+        param_distributions=param_distributions,
+        n_iter=iterations,
         scoring="neg_mean_squared_error",
+        n_jobs=jobs,
         cv=3,
         refit=False,
         verbose=10,
@@ -118,7 +122,13 @@ def main():
 
     X_dev, X_eval, y_dev, y_eval = train_test_split(X, y, train_size=0.9)
 
-    best_params = grid_search(X_dev, y_dev, training_epochs=args.epochs)
+    best_params = randomized_search(
+        X_dev,
+        y_dev,
+        training_epochs=args.epochs,
+        iterations=args.iterations,
+        jobs=args.jobs,
+    )
     print(f"Best params: {best_params}")
 
     model = NNEstimator(**best_params).fit(X_dev, y_dev, training_epochs=5)
@@ -127,13 +137,29 @@ def main():
 
 
 def parse_command_line() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("ss_filename")
-    parser.add_argument("sequence_ids_filename")
-    parser.add_argument("a_plus_deg_rates_filename")
-    parser.add_argument("a_minus_deg_rates_filename")
-    parser.add_argument("model", choices=("a+", "a-"))
-    parser.add_argument("--epochs", type=int, default=1)
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "ss_filename", help="File containing secondary structure information."
+    )
+    parser.add_argument("sequence_ids_filename", help="File containing sequence IDs.")
+    parser.add_argument(
+        "a_plus_deg_rates_filename", help="File containing A+ degradation rates."
+    )
+    parser.add_argument(
+        "a_minus_deg_rates_filename", help="File containing A- degradation rates."
+    )
+    parser.add_argument(
+        "model", choices=("a+", "a-"), help="Degradation model to evaluate."
+    )
+    parser.add_argument("--epochs", type=int, default=1, help="NN training epochs.")
+    parser.add_argument(
+        "--iterations", type=int, default=30, help="NN parameter combinations to test."
+    )
+    parser.add_argument(
+        "--jobs", type=int, default=1, help="Number of jobs to run in parallel."
+    )
 
     return parser.parse_args()
 
