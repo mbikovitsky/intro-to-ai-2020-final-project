7553a57de57d9dd8ea6f5ebc99634527d0f8cb8c
Michael Bikovitsky <biko@bikodbg.com>
2020-08-09 20:05:22 +0300

    Add utilities for various NN layer shape calculations

--- util.py
+++ util.py
@@ -43,6 +43,133 @@ def match_parens(string: str, dtype=np.uint8) -> np.ndarray:
     return pairs_matrix
 
 
+def conv_1d_output_length(
+    length_in: int,
+    kernel_size: int,
+    padding: int = 0,
+    dilation: int = 1,
+    stride: int = 1,
+) -> int:
+    """
+    Calculates the length of the output tensor for a 1D convolutional layer.
+
+    See https://pytorch.org/docs/stable/generated/torch.nn.Conv1d.html for more
+    information about the arguments to this function.
+
+    :param length_in:   Length of the input tensor.
+    :param kernel_size: Size of the convolution kernel.
+    :param padding:     Padding applied to the convolution.
+    :param dilation:    Dilation applied to the convolution.
+    :param stride:      Convolution stride.
+    """
+    return conv_2d_output_length(
+        length_in, length_in, kernel_size, padding, dilation, stride
+    )[0]
+
+
+def conv_2d_output_length(
+    height_in: int,
+    width_in: int,
+    kernel_size: Union[int, Tuple[int, int]],
+    padding: Union[int, Tuple[int, int]] = 0,
+    dilation: Union[int, Tuple[int, int]] = 1,
+    stride: Union[int, Tuple[int, int]] = 1,
+) -> Tuple[int, int]:
+    """
+    Calculates the width and height of the output tensor for a 2D convolutional layer.
+
+    See https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html for more
+    information about the arguments to this function.
+
+    :param height_in:   Height of the input tensor.
+    :param width_in:    Width of the input tensor.
+    :param kernel_size: Size of the convolution kernel.
+    :param padding:     Padding applied to the convolution.
+    :param dilation:    Dilation applied to the convolution.
+    :param stride:      Convolution stride.
+
+    :return: A tuple of the output height and width.
+    """
+    if not isinstance(kernel_size, Tuple):
+        kernel_size = (kernel_size, kernel_size)
+    if not isinstance(padding, Tuple):
+        padding = (padding, padding)
+    if not isinstance(dilation, Tuple):
+        dilation = (dilation, dilation)
+    if not isinstance(stride, Tuple):
+        stride = (stride, stride)
+
+    def formula(height_or_width: int, index: int) -> int:
+        return int(
+            (
+                height_or_width
+                + 2 * padding[index]
+                - dilation[index] * (kernel_size[index] - 1)
+                - 1
+            )
+            / stride[index]
+            + 1
+        )
+
+    height_out = formula(height_in, 0)
+    width_out = formula(width_in, 1)
+
+    return height_out, width_out
+
+
+def max_pool_2d_output_length(
+    height_in: int,
+    width_in: int,
+    kernel_size: Union[int, Tuple[int, int]],
+    padding: Union[int, Tuple[int, int]] = 0,
+    dilation: Union[int, Tuple[int, int]] = 1,
+    stride: Optional[Union[int, Tuple[int, int]]] = None,
+) -> Tuple[int, int]:
+    """
+    Calculates the width and height of the output tensor for a 2D max pooling layer.
+
+    See https://pytorch.org/docs/stable/generated/torch.nn.MaxPool2d.html for more
+    information about the arguments to this function.
+
+    :param height_in:   Height of the input tensor.
+    :param width_in:    Width of the input tensor.
+    :param kernel_size: Size of the pooling kernel.
+    :param padding:     Padding applied to the pooling.
+    :param dilation:    Dilation applied to the pooling.
+    :param stride:      Pooling stride.
+
+    :return: A tuple of the output height and width.
+    """
+    if not isinstance(kernel_size, Tuple):
+        kernel_size = (kernel_size, kernel_size)
+    if not isinstance(padding, Tuple):
+        padding = (padding, padding)
+    if not isinstance(dilation, Tuple):
+        dilation = (dilation, dilation)
+
+    if stride is None:
+        stride = kernel_size
+    if not isinstance(stride, Tuple):
+        stride = (stride, stride)
+
+    def formula(height_or_width: int, index: int) -> int:
+        return int(
+            (
+                height_or_width
+                + 2 * padding[index]
+                - dilation[index] * (kernel_size[index] - 1)
+                - 1
+            )
+            / stride[index]
+            + 1
+        )
+
+    height_out = formula(height_in, 0)
+    width_out = formula(width_in, 1)
+
+    return height_out, width_out
+
+
 def train_network(
     network: nn.Module,
     data_loader: DataLoader,
